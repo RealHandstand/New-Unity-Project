a0235c32c37a06b47edd1d379a0f182b303124f6
AH <addisondheath@gmail.com>
2021-11-18 08:45:58 +1100

    Main

---
+++ Assets/Free Assets-20211112T052813Z-001/Free Assets/AttackManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    public Animator playerAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            DirectionCheck();
+        }
+    }
+
+    void DirectionCheck()
+    {
+        if(Input.GetAxis("Mouse X") < 0 && Input.GetAxis("Mouse Y") < 0.15f && Input.GetAxis("Mouse Y") > -0.15f)
+        {
+            playerAnimator.SetTrigger("AttackRight");
+        }
+        else if (Input.GetAxis("Mouse X") > 0 && Input.GetAxis("Mouse Y") < 0.15f && Input.GetAxis("Mouse Y") > -0.15f)
+        {
+            playerAnimator.SetTrigger("AttackLeft");
+        }
+        else if (Input.GetAxis("Mouse Y") > 0 && Input.GetAxis("Mouse X") < 0.15f && Input.GetAxis("Mouse X") > -0.15f)
+        {
+            playerAnimator.SetTrigger("AttackStab");
+        }
+        else if (Input.GetAxis("Mouse Y") < 0 && Input.GetAxis("Mouse X") < 0.15f && Input.GetAxis("Mouse X") > -0.15f)
+        {
+            playerAnimator.SetTrigger("AttackUp");
+        }
+    }
+}
+
---
+++ Assets/Free Assets-20211112T052813Z-001/Free Assets/ToggleLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : MonoBehaviour
+{
+    public Light light;
+    bool lightOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(lightOn == false)
+            {
+                light.enabled = true;
+                lightOn = true;
+            }
+            else
+            {
+                light.enabled = false;
+                lightOn = false;
+            }
+        }
+    }
+}
